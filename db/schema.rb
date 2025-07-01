b3a5af9001b0f440b6d67203e05a1fb51a60d90a
Ash Goren <ash@mortalwombat.net>
2025-07-01 15:13:05 -0700

    update gitignore

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 
 ActiveRecord::Schema[7.0].define(version: 2023_09_12_201707) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "pg_stat_statements"
   enable_extension "plpgsql"
 
   create_table "attendance_records", id: :serial, force: :cascade do |t|
