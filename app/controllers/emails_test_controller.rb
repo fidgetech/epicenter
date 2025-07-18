db5b0dd48cbecd01ed96ba515d99b368d71d94d9
Ash Goren <ash@mortalwombat.net>
2025-07-18 14:36:42 -0700

    add email test

---
+++ app/controllers/emails_test_controller.rb
@@ -0,0 +1,13 @@
+# TESTING ONLY
+class EmailsTestController < ApplicationController
+  def send_test
+    EmailJob.perform_later(
+      { :from => ENV['FROM_EMAIL_REVIEW'],
+        :to => '<test@mortalwombat.net>',
+        :subject => 'Test Email',
+        :body => 'This is a test email sent from the EmailsTestController.'
+      }
+    )
+    redirect_to courses_path, notice: 'Test email queued successfully.'
+  end
+end
